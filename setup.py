4b7336f4310a69633de67c30d64015da8d181807
Ellandor <hibernatingalltime.kkk@protonmail.com>
2021-04-15 08:27:27 -0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as stream:
 
 setup(
     name='Amsync',
-    version='0.0.15',
+    version='0.0.16',
     url='https://github.com/ellandor/Amsync',
     license='MIT',
     author='SempreLegit',
