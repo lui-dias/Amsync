56706a5c5a56b02b9b0b001658773e1e0ee111ea
Ellandor <hibernatingalltime.kkk@protonmail.com>
2021-04-20 09:55:08 -0300

    Add more exceptions

--- amsync/exceptions.py
+++ amsync/exceptions.py
@@ -1,10 +1,8 @@
-class SmallReasonForBan(Exception):
-    pass
-
-
-class AccountNotFoundInDotenv(Exception):
-    pass
-
-
-class InvalidDotenvKeys(Exception):
-    pass
+class SmallReasonForBan(Exception): pass
+class AccountNotFoundInDotenv(Exception): pass
+class InvalidDotenvKeys(Exception): pass
+class CommandIsntAsync(Exception): pass
+class EventIsntAsync(Exception): pass
+class InvalidChatChoice(Exception): pass
+class InvalidEvent(Exception): pass
+class InvalidFileType(Exception): pass
