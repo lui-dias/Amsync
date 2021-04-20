0c46ef7809c5e976d5586481971cc8ef4eb2df16
Ellandor <hibernatingalltime.kkk@protonmail.com>
2021-04-20 10:01:49 -0300

    Refactor

--- amsync/ws.py
+++ amsync/ws.py
@@ -1,17 +1,17 @@
 from __future__ import annotations
 
-from re import search
 from typing import Any, Callable, AsyncIterator, Coroutine
-from asyncio import get_event_loop
+from asyncio import get_event_loop, AbstractEventLoop, Future
 from contextlib import suppress
 
 from ujson import dumps, loads
 from aiohttp import ClientSession
 from pybase64 import urlsafe_b64decode
 
-from . import obj  # type: ignore
-from .db import DB  # type: ignore
-from .obj import Message, req  # type: ignore
+from . import obj
+from .db import DB
+from .obj import Message, _req
+from .enum import WsStatus
 
 Coro_return_None = Callable[[], Coroutine[None, None, None]]
 Coro_return_Message = Callable[[], Coroutine[Message, None, None]]
@@ -20,24 +20,29 @@ Coro_return_Any = Callable[[], Coroutine[Any, None, None]]
 
 class Ws:
     def __init__(
-        self, email: str, password: str, only_chats={}, ignore_chats={}
+        self,
+        email:        str,
+        password:     str,
+        only_chats:   dict[str, list[str]] = {},
+        ignore_chats: dict[str, list[str]] = {}
     ):
-        self._deviceid = obj.headers['NDCDEVICEID']
-        self._email = email
-        self._password = password
-        self._db = DB()
-        self._loop = get_event_loop()
-        self._only_chats = only_chats
+        self._deviceid: str               = obj.headers['NDCDEVICEID']
+        self._loop:     AbstractEventLoop = get_event_loop()
+        self._db:       DB                = DB()
+        self.futures:   list[Future]      = []
+
+        self._email        = email
+        self._password     = password
+        self._only_chats   = only_chats
         self._ignore_chats = ignore_chats
-        self.futures = []
+
 
     async def _get_sid(self) -> str:
-        data = {
-            'email': self._email,
-            'secret': f'0 {self._password}',
+        return (await _req('post', 'g/s/auth/login', data={
+            'email':    self._email,
+            'secret':  f'0 {self._password}',
             'deviceID': self._deviceid,
-        }
-        return (await req('post', 'g/s/auth/login', data=data))['sid']
+        }))['sid']
 
     async def _connect(self) -> AsyncIterator[Message]:
         async with ClientSession(json_serialize=dumps) as session:
@@ -45,18 +50,23 @@ class Ws:
                 f'wss://ws1.narvii.com/?signbody={self._deviceid}',
                 headers=obj.headers,
             )
-            for i in self._events['ready']:
-                self._loop.create_task(i())
+            self._call_events('ready')
 
             while True:
                 if ws.closed:
-                    for i in self._events['close']:
-                        self._loop.create_task(i())
-                    yield False
+                    self._call_events('close')
+                    yield WsStatus.CLOSED
+
+                # The socket sometimes receives a frame that is not a json,
+                # causing a TypeError, ignoring it does not cause any problems
                 with suppress(TypeError):
                     res = await ws.receive_json(loads=loads)
                     if res['t'] == 1000:
                         yield Message().from_ws(res['o'])
+
+    def _call_events(self, event):
+        for i in self._events[event]:
+            self._loop.create_task(i())
 
     def _can_call(self, msg: Message):
         if not self._only_chats and not self._ignore_chats:
@@ -80,45 +90,43 @@ class Ws:
 
     async def run(
         self,
-        call: Coro_return_None,
-        events: dict[
-            str,
-            list[Coro_return_Any],
-        ],
-        bot,
+        call:   Coro_return_None,
+        events: dict[str, list[Coro_return_Any]],
+        bot:    'Bot' # type: ignore
     ) -> None:
         if not self._db.get_account(self._email):
             self._db.add_account(self._email, await self._get_sid())
 
-        # Sometimes sid can be an invalid base64, causing a padding error in urlsafe_b64decode
-        # Adding == at the end of sid solves the problem
+        # Sometimes sid can be an invalid base64,
+        # causing a padding error in urlsafe_b64decode
+        # Add = until sid has the len of 192 solve the problem
         tmp = self._db.get_account(self._email)
         sid = tmp + '=' * (192 - len(tmp))
 
         obj.headers['NDCAUTH'] = f'sid={sid}'
-        id_ = search(
-            r'\w{8}-\w{4}-\w{4}-\w{4}-\w{12}',
-            urlsafe_b64decode(sid).decode('cp437'),
-        ).group(0)
+        id_ = loads(
+            urlsafe_b64decode(sid[:-28])[1:] + b'}'
+        )['2']
+
         bot.id = id_
         obj.bot_id = id_
 
         self._events = events
+        events = {
+            '0:0':   'message',
+            '100:0': 'message',
+            '101:0': 'join_chat',
+            '102:0': 'leave_chat',
+            '0:100': 'image',
+        }
 
         async for m in self._connect():
-            if not m:
-                return await self.run(call, events, bot)
+            if m == WsStatus.CLOSED:
+                return await self.run(call, self._events, bot)
+
             if self._can_call(m):
                 with suppress(KeyError):
-                    for i in {
-                        '0:0': self._events['message'],
-                        '100:0': self._events['message'],
-                        '101:0': self._events['join_chat'],
-                        '102:0': self._events['leave_chat'],
-                        '0:100': self._events['image'],
-                    }[f'{m.type}:{m.media_type}']:
-
-                        self._loop.create_task(i(m))
+                    self._call_events(events[f'{m.type}:{m.media_type}'])
 
                 if self.futures:
                     for future in self.futures:
