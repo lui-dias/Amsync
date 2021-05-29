9839b91492d05cc80252c04ac263370ba52afa54
ellandor <66072698+ellandor@users.noreply.github.com>
2021-05-29 16:57:39 -0300

    Update version 0.0.49

--- amsync/__init__.py
+++ amsync/__init__.py
@@ -6,7 +6,7 @@ from .image import *
 from .dataclass import *
 from .exceptions import InvalidPythonVersion
 
-__version__ = '0.0.47'
+__version__ = '0.0.49'
 
 if sys.version_info < (3, 8):
     raise InvalidPythonVersion(f"Your python {'.'.join(map(str, sys.version_info[:2]))}, use python >= 3.8")
