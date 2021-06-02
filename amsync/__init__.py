efddb2f0d7c8faabba79f85c95d6cd96bd98eb7b
ellandor <66072698+ellandor@users.noreply.github.com>
2021-06-02 12:27:27 -0300

    feat: __version__ for `Test` and `Production`

--- amsync/__init__.py
+++ amsync/__init__.py
@@ -6,7 +6,12 @@ from .image import *
 from .dataclass import *
 from .exceptions import InvalidPythonVersion
 
-__version__ = '0.0.50'
+# Production
+__version__ = '0.0.51'
+
+# Test
+#__version__ = '0.0.51'
+
 
 if sys.version_info < (3, 8):
     raise InvalidPythonVersion(f"Your python {'.'.join(map(str, sys.version_info[:2]))}, use python >= 3.8")
